d153e862fb445aa77752df950d19046320ce01e0
西澤　康造 <Kozo@mba.local>
2019-12-12 22:44:58 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.assets.unknown_asset_fallback = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = #'https://you-46.com'
+  config.action_controller.asset_host = 'https://mabudachi.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
