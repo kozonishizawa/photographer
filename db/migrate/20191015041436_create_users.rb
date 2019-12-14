dc1cb0e86fc54e074fdb603c7dd7469e0d15f63c
西澤　康造 <Kozo@mba.local>
2019-12-14 22:13:24 +0900

    fix

--- db/migrate/20191015041436_create_users.rb
+++ db/migrate/20191015041436_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :users, options: 'DEFAULT CHARSET=utf8' do |t|
       t.string :name, null: false
       t.string :email, null: false
       t.string :tel, null:false
--- db/migrate/20191024223155_add_index_to_users_email.rb
+++ db/migrate/20191024223155_add_index_to_users_email.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[6.0]
   def change
-    add_index :users, :email, unique: true, name: :unko
+    add_index :users, :email, unique: true, name: :index_user_email
   end
 end
