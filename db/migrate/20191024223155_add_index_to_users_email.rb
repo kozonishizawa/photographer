0dd81821b6d8fc67e575b2deeda12cb0d9526186
西澤　康造 <Kozo@mba.local>
2019-12-14 20:52:09 +0900

    fix

--- db/migrate/20191024223155_add_index_to_users_email.rb
+++ db/migrate/20191024223155_add_index_to_users_email.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[6.0]
   def change
-    add_index :users, :email, unique: true, name: :index_users_email
+    add_index :users, :email, :unique => true, :name => 'index_users_email'
   end
 end
