cb8753664785eaac04c643d2f0b7cc3af1f23d87
西澤　康造 <Kozo@mba.local>
2019-12-14 12:13:54 +0900

    fix

--- db/migrate/20191024223155_add_index_to_users_email.rb
+++ db/migrate/20191024223155_add_index_to_users_email.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[6.0]
   def change
-    add_index :users, :email, unique: true, options: 'DEFAULT CHARSET=utf8'
+    add_index :users, :email, unique: true, name: :index_users_email
   end
 end
