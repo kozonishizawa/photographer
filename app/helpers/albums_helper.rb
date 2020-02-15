82b2212e2d72166e8cc5f8dc2b0c91b0e1f37c09
西澤　康造 <Kozo@mba.local>
2020-02-15 21:13:01 +0900

    [modify] albums_helperの復活

---
+++ app/helpers/albums_helper.rb
@@ -0,0 +1,13 @@
+module AlbumsHelper
+
+  def downloadable_counter
+    selected = Photo.selected.joins(album: :user).merge(User.where(id: current_user.id)).count
+    selectable = current_user.downloadable_limit - selected
+    if selectable >= 0
+      return selectable
+    else 
+      return "ダウンロード可能上限を超えています。"
+    end
+  end
+
+end 
